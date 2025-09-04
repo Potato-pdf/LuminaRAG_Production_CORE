ee93aa1dfa2ee4aaeee4074e1df1407a2f2fc821
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-04 11:15:31 -0600

    feat:  cambio de modelo

--- src/embedding/multilingual/choice_embedding.py
+++ src/embedding/multilingual/choice_embedding.py
@@ -1,5 +1,5 @@
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from src.embedding.pooling.pooled_hugging_face_embendding import PooledHuggingFaceEmbedding
 
 def choice_embedding():
-    embedding_model = HuggingFaceEmbedding(model_name="intfloat/multilingual-e5-base")
+    embedding_model = PooledHuggingFaceEmbedding(model_name="efederici/e5-base-multilingual-4096")
     return embedding_model
