f2daa692b2420478b9e2abaf2e306f665706c78b
joel_briones <joellbriones701@gmail.com>
2025-09-08 11:33:25 -0600

    add enviroment

--- src/model_ai/choice_model_llama.py
+++ src/model_ai/choice_model_llama.py
@@ -3,5 +3,5 @@ from langchain_community.llms import Ollama
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 def connect_ollama():
-    llm = Ollama(model="llama3.2", base_url="http://localhost:11435")
+    llm = Ollama(model="llama3.2", base_url="http://localhost:3203")
     return llm
