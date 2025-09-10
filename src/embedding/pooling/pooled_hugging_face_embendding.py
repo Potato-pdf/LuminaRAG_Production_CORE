a26508b4736d8b29025996313fe336b338982785
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-10 13:19:08 -0600

    feat: documentacioooooooon

--- src/embedding/pooling/pooled_hugging_face_embendding.py
+++ src/embedding/pooling/pooled_hugging_face_embendding.py
@@ -5,17 +5,16 @@ from src.config import EMBEDDING_CONFIG
 
 class PooledHuggingFaceEmbedding:
     def __init__(self, model_name: str):
-        self.base_model = HuggingFaceEmbedding(model_name=model_name)
-        self.embed_dim = EMBEDDING_CONFIG["embedding_dim"]
+        self.base_model = HuggingFaceEmbedding(model_name=model_name)#| modelo para embendings
+        self.embed_dim = EMBEDDING_CONFIG["embedding_dim"]#| dimencion del embending
     
-    def get_text_embedding(self, text: str) -> List[float]:
+    def get_text_embedding(self, text: str) -> List[float]:# | crea el embending de un texto
         token_embeddings = self.base_model._get_raw_embedding(text)
-        
-        mean_pooled = np.mean(token_embeddings, axis=0).tolist()
+        mean_pooled = np.mean(token_embeddings, axis=0).tolist()#| saca el promedio columna por columna y lo almacena en una lista
         return mean_pooled
     
     def get_text_embedding_batch(self, texts: List[str]) -> List[List[float]]:
         results = []
         for text in texts:
             results.append(self.get_text_embedding(text))
-        return results
+        return results  
