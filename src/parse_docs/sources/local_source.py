4b9bcf4e85e64942431a759d603c5c6046da9f19
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-10 12:15:29 -0600

    feat: busqueda en archivos locales

---
+++ src/parse_docs/sources/local_source.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import List, Optional
+from src.config import PATH_CONFIG, PARSING_CONFIG
+
+class LocalFileSource:
+
+    def __init__(self, directory_path: str = None):
+        self.directory_path = PATH_CONFIG["pdf_directory"]
+
+    def get_files_path(self) -> List[Path]:
+        doc_dir = Path(self.directory_path)
+        all_files = []
+        for ext in PARSING_CONFIG["supported_extensions"]:
+            all_files.extend(doc_dir.glob(f"*{ext}"))
+        
+        if not all_files:
+            print(f"No se encontraron archivos {PARSING_CONFIG['supported_extensions']} en {self.directory_path}")
+        
+        return all_files
